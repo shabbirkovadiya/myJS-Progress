1f2f864d3a47dc4a294688c7e098bb77215cedd3
shabbirkovadiya <shabbirkovadiya07@gmail.com>
2025-06-10 12:27:09 +0530

    JS All General Tasks Completed

--- Extra task/JS General Tasks/main.js
+++ Extra task/JS General Tasks/main.js
@@ -24,7 +24,7 @@ const myFunc3 = (arr1, arr2) => {
 document.getElementById("ex3").innerText = myFunc3(['a', 'b', 'c', 'd', 'e'], [1, 3]);
 
 //exercise 4
-const myFunc4 = str => str.replace(str.slice(4, -12), "...");
+const myFunc4 = str => str.replace(str.split("@")[0].slice(4),"...");
 document.getElementById("ex4").innerText = myFunc4("dcoders_Iab@exampIe.com");
 
 //exercise 5
@@ -32,16 +32,11 @@ const myFunc5 = arr => arr.filter(e => typeof e == "number").reduce((sum, e) => 
 document.getElementById("ex5").innerText = myFunc5([15, 35, -5, "a", 0, -15]);
 
 // exercise 6
-// const myFunc6 = num => {
-// Pendingg 
-// }
-// document.getElementById("ex6").innerText = myFunc6(32);
+const myFunc6 = num => Math.ceil(num / 5) * 5;
+document.getElementById("ex6").innerText = myFunc6(32);
 
 // exercise 7
-const myFunc7 = (arr, i) => {
-  arr.splice(i, 1);
-  return arr;
-}
+const myFunc7 = (arr, i) => arr.toSpliced(i, 1);
 document.getElementById("ex7").innerText = JSON.stringify(myFunc7([{ x: 1 }, { y: 2 }, { z: 3 }], 1));
 
 // exercise 8 
@@ -58,6 +53,8 @@ const myFunc9 = (arr, value) => arr.filter(e => e <= value).length;
 document.getElementById("ex9").innerText = myFunc9([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5);
 
 // exercise 10
-const myFunc10 = arr => arr.sort((a,b) => a.price - b.price)
-document.getElementById("ex10").innerText =JSON.stringify( myFunc10([{product: "phone", price:15000}, {product: "laptop",
-price:75000}, {product: "camera", price:12000}]));
+const myFunc10 = arr => arr.sort((a, b) => a.price - b.price)
+document.getElementById("ex10").innerText = JSON.stringify(myFunc10([{ product: "phone", price: 15000 }, {
+  product: "laptop",
+  price: 75000
+}, { product: "camera", price: 12000 }]));
